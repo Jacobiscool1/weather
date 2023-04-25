d7fa6d0ad60e1ac1f8e61ad959631ba99cd01006
Jacobiscool1 <jao2156132@maricopa.edu>
2023-04-25 13:41:05 -0700

    weatherappstart

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [location, setLocation] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      }, (error) => {
+        setError(error.message);
+      });
+    } else {
+      setError('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      const { latitude, longitude } = location;
+      const apiKey = 'fc49c842aa87427e812eb58b2c735a80';
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+      axios.get(apiUrl)
+        .then(response => {
+          setWeatherData(response.data);
+        })
+        .catch(error => {
+          setError(error.message);
+        });
+    }
+  }, [location]);
+
+  return (
+    <div className="App">
+      {error && <div>Error: {error}</div>}
+      {location && <div>Location: {location.latitude}, {location.longitude}</div>}
+      {weatherData && (
+        <div>
+          <div>Current Temperature: {weatherData.main.temp}°C</div>
+          <div>Weather Description: {weatherData.weather[0].description}</div>
+          <div>Humidity: {weatherData.main.humidity}%</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
